f70ba733b319e5b58364619d298d0a8a74fc71c5
Aidan <beckeraidan9@gmail.com>
2025-10-06 23:43:02 -0400

    Finish day1 part 1

---
+++ day1/main.py
@@ -0,0 +1,26 @@
+
+
+with open("input.txt") as file:
+    arr1 = []
+    arr2 = []
+
+    for line in file:
+        out = line.split()
+        
+        arr1.append(int(out[0]))
+        arr2.append(int(out[1]))
+
+    arr1.sort()
+    arr2.sort()
+
+    total_value = 0
+
+    for i in range(len(arr1)):
+        difference = arr1[i] - arr2[i]
+
+        if(difference < 0):
+            difference *= -1
+
+        total_value += difference
+
+    print(total_value)
