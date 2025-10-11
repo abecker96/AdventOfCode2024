8c00e765d606a3ccec697a04e763d2936e61752e
Aidan <beckeraidan9@gmail.com>
2025-10-11 13:53:41 -0400

    Finish day3 part 1. Replace manual search with regex matching.

--- day3/part1/main.py
+++ day3/part1/main.py
@@ -1,3 +1,5 @@
+import re
+
 
 startString = "mul("
 endString = ")"
@@ -30,27 +32,61 @@ def main():
     with open("input.txt") as file:
 
         for line in file:
-            while(len(line)):
-                index = line.find(startString)
+            # Definitely better to just replace this with a regex search
+            # this is a terribly inefficient first pass, doing way more string
+            # copying than necessary
+            # But it could probably be made to be more efficient
+            # than the regex with a little effort
 
-                if(index != -1):
-                    # Found
-                    line = line[index + len(startString):]
+            # while(len(line)):
+            #     print(f"Start of loop, line: {line}")
+            #     index = line.find(startString)
 
-                val1 = parseDigits(line)
-                index = line.find(sep)
-                line = line[index+1:]
-                val2 = parseDigits(line)
+            #     if(index != -1):
+            #         # Found, delete the next part of the string
+            #         line = line[index + len(startString):]
+            #     else:
+            #         break
 
-                if(val1 > 0 and val2 > 0 and index != -1 and index <= maxDigits):
-                    print(f"Val1: {val1}, Val2: {val2}, Index: {index}")
+            #     val1 = parseDigits(line)
+            #     if(val1 <= 0):
+            #         continue
+                    
+            #     index = line.find(sep)
+            #     if(index <= 0 or index > maxDigits):
+            #         continue
                 
-                index = line.find(endString)
+            #     line = line[index+1:]
+
+            #     val2 = parseDigits(line)
+            #     if(val2 <= 0):
+            #         continue
                 
-                if(index != -1 and index <= maxDigits):
-                    sum += val1 * val2
-                    line = line[index+1:]
-                    continue
+            #     index = line.find(endString)
+            #     if(index <= 0 or index > maxDigits):
+            #         continue
+                
+            #     line = line[index+1:]
+            #     print(f"Val1: {val1}, Val2: {val2}")
+            #     sum += val1 * val2
+
+            # Match any occurrence of the term "mul(d,d)"
+            # Where 'd' is 1-3 decimal digits, s.t. the 
+            # first digit is not 0
+            regex = re.compile(r'mul\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\)')
+            # Find all non-overlapping valid strings in the line
+            matches = regex.findall(line)
+
+            # Get a string containing decimal digits
+            # This one can be simpler, the first regex already
+            # made sure that the string has a valid number of digits
+            digitregex = re.compile(r'\d+')
+            
+            for match in matches:
+                # Get digits out of the string
+                digits = digitregex.findall(match)
+                # Array accesses are find, we've already validated the string
+                sum += int(digits[0]) * int(digits[1])
 
 
         print(sum)
@@ -59,4 +95,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-#correct answer was 354
+#correct answer was 173419328
